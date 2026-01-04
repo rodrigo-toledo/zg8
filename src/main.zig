5869814864a672480238f2b09d804639a0cdbc26
Rodrigo Toledo <20404433+rodrigo-toledo@users.noreply.github.com>
2026-01-04 20:20:50 +0100

    implemented more op codes

--- src/main.zig
+++ src/main.zig
@@ -29,7 +29,7 @@ const Cpu = struct {
     sp: u4,
     delay_timer: u8,
     sound_timer: u8,
-    keypad: [keys]u8,
+    keypad: [keys]u1,
     video: [video_rows][video_columns]bool, // guide uses u32 for compat with SDL
 
     pub fn init() Cpu {
@@ -98,6 +98,26 @@ const Cpu = struct {
             0x00EE => self.Op00EE(),
             0x1000...0x1FFF => self.Op1nnn(opcode),
             0x2000...0x2FFF => self.Op2nnn(opcode),
+            0x3000...0x3FFF => self.Op3xkk(opcode),
+            0x4000...0x4FFF => self.Op4xkk(opcode),
+            0x5000...0x5FFF => self.Op5xy0(opcode), //TODO
+            0x6000...0x6FFF => self.Op6xkk(opcode),
+            0x7000...0x7FFF => self.Op7xkk(opcode),
+            0x8000...0x8FFF => {
+                switch (Decode.n(opcode)) {
+                    0 => self.Op8xy0(opcode),
+                    1 => self.Op8xy1(opcode),
+                    2 => self.Op8xy2(opcode),
+                    3 => self.Op8xy3(opcode),
+                    4 => self.Op8xy4(opcode),
+                    5 => self.Op8xy5(opcode),
+                    6 => self.Op8xy6(opcode),
+                    7 => self.Op8xy7(opcode),
+                    0xE => self.Op8xyE(opcode),
+                }
+            },
+            0x7000...0x7FFF => self.Op7xkk(opcode),
+            0x7000...0x7FFF => self.Op7xkk(opcode),
 
             else => return error.UnknownOpcode,
         }
@@ -106,14 +126,14 @@ const Cpu = struct {
 
     /// 00E0 - CLS
     /// Clear the display.
-    pub fn Op00E0(self: *Cpu) !void {
+    fn Op00E0(self: *Cpu) !void {
         self.video = std.mem.zeroes([video_rows][video_columns]bool);
     }
 
     ///00EE - RET
     ///Return from a subroutine.
     ///The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack pointer.
-    pub fn Op00EE(self: *Cpu) !void {
+    fn Op00EE(self: *Cpu) !void {
         self.sp -= 1;
         self.pc = self.stack[self.sp];
     }
@@ -122,7 +142,7 @@ const Cpu = struct {
     ///Jump to location nnn.
     ///
     ///The interpreter sets the program counter to nnn.
-    pub fn Op1nnn(self: *Cpu, opcode: Opcode) !void {
+    fn Op1nnn(self: *Cpu, opcode: Opcode) !void {
         self.pc = Decode.nnn(opcode);
     }
 
@@ -130,7 +150,7 @@ const Cpu = struct {
     ///Call subroutine at nnn.
     ///
     ///The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
-    pub fn Op2nnn(self: *Cpu, opcode: Opcode) !void {
+    fn Op2nnn(self: *Cpu, opcode: Opcode) !void {
         self.sp += 1;
         self.stack[self.sp] = self.pc;
 
@@ -141,156 +161,284 @@ const Cpu = struct {
     ///Skip next instruction if Vx = kk.
     ///
     ///The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
-    pub fn Op3xkk(self: *Cpu, opcode: Opcode) !void {
+    fn Op3xkk(self: *Cpu, opcode: Opcode) !void {
         if (self.registers[Decode.x(opcode)] == Decode.nn(opcode)) {
             self.pc += 2;
         }
     }
+
     ///4xkk - SNE Vx, byte
     ///Skip next instruction if Vx != kk.
     ///
     ///The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
-    pub fn Op4xkk(self: *Cpu, opcode: Opcode) !void {}
+    fn Op4xkk(self: *Cpu, opcode: Opcode) !void {
+        if (self.registers[Decode.x(opcode)] != Decode.nn(opcode)) {
+            self.pc += 2;
+        }
+    }
     ///5xy0 - SE Vx, Vy
     ///Skip next instruction if Vx = Vy.
     ///
     ///The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
-    pub fn Op5xy0(self: *Cpu, opcode: Opcode) !void {}
+    fn Op5xy0(self: *Cpu, opcode: Opcode) !void {
+        if (self.registers[Decode.x(opcode)] != self.registers[Decode.y(opcode)]) {
+            self.pc += 2;
+        }
+    }
+
     ///6xkk - LD Vx, byte
     ///Set Vx = kk.
     ///
     ///The interpreter puts the value kk into register Vx.
-    pub fn Op6xkk(self: *Cpu, opcode: Opcode) !void {}
+    fn Op6xkk(self: *Cpu, opcode: Opcode) !void {
+        self.registers[Decode.x(opcode)] = Decode.nn(opcode);
+    }
+
     ///7xkk - ADD Vx, byte
     ///Set Vx = Vx + kk.
     ///
     ///Adds the value kk to the value of register Vx, then stores the result in Vx.
-    pub fn Op7xkk(self: *Cpu, opcode: Opcode) !void {}
+    fn Op7xkk(self: *Cpu, opcode: Opcode) !void {
+        self.registers[Decode.x(opcode)] += Decode.nn(opcode);
+    }
+
     ///8xy0 - LD Vx, Vy
     ///Set Vx = Vy.
     ///
     ///Stores the value of register Vy in register Vx.
-    pub fn Op8xy0(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xy0(self: *Cpu, opcode: Opcode) !void {
+        self.registers[Decode.x(opcode)] += self.registers[Decode.y(opcode)];
+    }
+
     ///8xy1 - OR Vx, Vy
     ///Set Vx = Vx OR Vy.
     ///
     ///Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. A bitwise OR compares the corrseponding bits from two values, and if either bit is 1, then the same bit in the result is also 1. Otherwise, it is 0.
-    pub fn Op8xy1(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xy1(self: *Cpu, opcode: Opcode) !void {
+        self.registers[Decode.x(opcode)] |= self.registers[Decode.y(opcode)];
+    }
+
     ///8xy2 - AND Vx, Vy
     ///Set Vx = Vx AND Vy.
     ///
     ///Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. A bitwise AND compares the corrseponding bits from two values, and if both bits are 1, then the same bit in the result is also 1. Otherwise, it is 0.
-    pub fn Op8xy2(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xy2(self: *Cpu, opcode: Opcode) !void {
+        self.registers[Decode.x(opcode)] &= self.registers[Decode.y(opcode)];
+    }
+
     ///8xy3 - XOR Vx, Vy
     ///Set Vx = Vx XOR Vy.
     ///
     ///Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx. An exclusive OR compares the corrseponding bits from two values, and if the bits are not both the same, then the corresponding bit in the result is set to 1. Otherwise, it is 0.
-    pub fn Op8xy3(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xy3(self: *Cpu, opcode: Opcode) !void {
+        self.registers[Decode.x(opcode)] ^= self.registers[Decode.y(opcode)];
+    }
+
     ///8xy4 - ADD Vx, Vy
     ///Set Vx = Vx + Vy, set VF = carry.
     ///
     ///The values of Vx and Vy are added together. If the result is greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
-    pub fn Op8xy4(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xy4(self: *Cpu, opcode: Opcode) !void {
+        const x = Decode.x(opcode);
+        const sum = @addWithOverflow(self.registers[x], self.registers[Decode.y(opcode)]);
+        self.registers[x] = sum[0];
+        self.registers[0xF] = sum[1];
+    }
+
     ///8xy5 - SUB Vx, Vy
     ///Set Vx = Vx - Vy, set VF = NOT borrow.
     ///
     ///If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
-    pub fn Op8xy5(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xy5(self: *Cpu, opcode: Opcode) !void {
+        const x = Decode.x(opcode);
+        const sum = @subWithOverflow(self.registers[x], self.registers[Decode.y(opcode)]);
+        self.registers[x] = sum[0];
+        self.registers[0xF] = -1 * sum[1];
+    }
+
     ///8xy6 - SHR Vx {, Vy}
     ///Set Vx = Vx SHR 1.
     ///
     ///If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
-    pub fn Op8xy6(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xy6(self: *Cpu, opcode: Opcode) !void {
+        const x = Decode.x(opcode);
+        const Vx = self.registers[x];
+        self.registers[0xF] = Vx & 1;
+        self.registers[x] >>= 1;
+    }
+
     ///8xy7 - SUBN Vx, Vy
     ///Set Vx = Vy - Vx, set VF = NOT borrow.
     ///
     ///If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
-    pub fn Op8xy7(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xy7(self: *Cpu, opcode: Opcode) !void {
+        const x = Decode.x(opcode);
+        const y = Decode.y(opcode);
+        const sum = @subWithOverflow(self.registers[y], self.registers[x]);
+        self.registers[x] = sum[0];
+        self.registers[0xF] = -1 * sum[1];
+    }
+
     ///8xyE - SHL Vx {, Vy}
     ///Set Vx = Vx SHL 1.
     ///
     ///If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
-    pub fn Op8xyE(self: *Cpu, opcode: Opcode) !void {}
+    fn Op8xyE(self: *Cpu, opcode: Opcode) !void {
+        const x = Decode.x(opcode);
+        const Vx = self.registers[x];
+        self.registers[0xF] = Vx & 1;
+        self.registers[x] <<= 1;
+    }
+
     ///9xy0 - SNE Vx, Vy
     ///Skip next instruction if Vx != Vy.
     ///
     ///The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
-    pub fn Op9xy0(self: *Cpu, opcode: Opcode) !void {}
+    fn Op9xy0(self: *Cpu, opcode: Opcode) !void {
+        if (self.GetX(opcode) != self.GetY(opcode)) {
+            self.pc += 2;
+        }
+    }
+
     ///Annn - LD I, addr
     ///Set I = nnn.
     ///
     ///The value of register I is set to nnn.
-    pub fn OpAnnn(self: *Cpu, opcode: Opcode) !void {}
+    fn OpAnnn(self: *Cpu, opcode: Opcode) !void {
+        self.index = Decode.nnn(opcode);
+    }
+
     ///Bnnn - JP V0, addr
     ///Jump to location nnn + V0.
     ///
     ///The program counter is set to nnn plus the value of V0.
-    pub fn OpBnnn(self: *Cpu, opcode: Opcode) !void {}
+    fn OpBnnn(self: *Cpu, opcode: Opcode) !void {
+        self.pc = Decode.nnn(opcode) + self.registers[0];
+    }
+
     ///Cxkk - RND Vx, byte
     ///Set Vx = random byte AND kk.
     ///
     ///The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx. See instruction 8xy2 for more information on AND.
-    pub fn OpCxkk(self: *Cpu, opcode: Opcode) !void {}
+    fn OpCxkk(self: *Cpu, opcode: Opcode) !void {
+        self.registers[Decode.x(opcode)] = rand() & Decode.nn(opcode);
+    }
+
     ///Dxyn - DRW Vx, Vy, nibble
     ///Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
     ///
     ///The interpreter reads n bytes from memory, starting at the address stored in I. These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). Sprites are XORed onto the existing screen. If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it is outside the coordinates of the display, it wraps around to the opposite side of the screen. See instruction 8xy3 for more information on XOR, and section 2.4, Display, for more information on the Chip-8 screen and sprites.
-    pub fn OpDxyn(self: *Cpu, opcode: Opcode) !void {}
+    fn OpDxyn(self: *Cpu, opcode: Opcode) !void {
+        _ = self;
+        _ = opcode; //TODO (this is the hardest one)
+    }
+
     ///Ex9E - SKP Vx
     ///Skip next instruction if key with the value of Vx is pressed.
     ///
     ///Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
-    pub fn OpEx9E(self: *Cpu, opcode: Opcode) !void {}
+    fn OpEx9E(self: *Cpu, opcode: Opcode) !void {
+        if (self.keypad[GetX(opcode)] == 1) {
+            self.pc += 2;
+        }
+    }
+
     ///ExA1 - SKNP Vx
     ///Skip next instruction if key with the value of Vx is not pressed.
     ///
     ///Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
-    pub fn OpExA1(self: *Cpu, opcode: Opcode) !void {}
+    fn OpExA1(self: *Cpu, opcode: Opcode) !void {
+        if (self.keypad[GetX(opcode)] == 0) {
+            self.pc += 2;
+        }
+    }
+
     ///Fx07 - LD Vx, DT
     ///Set Vx = delay timer value.
     ///
     ///The value of DT is placed into Vx.
-    pub fn OpFx07(self: *Cpu, opcode: Opcode) !void {}
+    fn OpFx07(self: *Cpu, opcode: Opcode) !void {
+        self.registers[Decode.x(opcode)] = self.delay_timer;
+    }
+
     ///Fx0A - LD Vx, K
     ///Wait for a key press, store the value of the key in Vx.
     ///
     ///All execution stops until a key is pressed, then the value of that key is stored in Vx.
-    pub fn OpFx0A(self: *Cpu, opcode: Opcode) !void {}
+    fn OpFx0A(self: *Cpu, opcode: Opcode) !void {
+        var i: usize = 0;
+
+        main: while (true) {
+            // Access current item
+            const current = self.keypad[i];
+            if (current == 1) {
+                self.registers[Decode.x(opcode)] = current;
+                break :main;
+            }
+
+            // Increment and Wrap
+            i = (i + 1) % self.keypad.len;
+        }
+    }
+
     ///Fx15 - LD DT, Vx
     ///Set delay timer = Vx.
     ///
     ///DT is set equal to the value of Vx.
-    pub fn OpFx15(self: *Cpu, opcode: Opcode) !void {}
+    fn OpFx15(self: *Cpu, opcode: Opcode) !void {}
+
     ///Fx18 - LD ST, Vx
     ///Set sound timer = Vx.
     ///
     ///ST is set equal to the value of Vx.
-    pub fn OpFx18(self: *Cpu, opcode: Opcode) !void {}
+    fn OpFx18(self: *Cpu, opcode: Opcode) !void {}
+
     ///Fx1E - ADD I, Vx
     ///Set I = I + Vx.
     ///
     ///The values of I and Vx are added, and the results are stored in I.
-    pub fn OpFx1E(self: *Cpu, opcode: Opcode) !void {}
+    fn OpFx1E(self: *Cpu, opcode: Opcode) !void {}
+
     ///Fx29 - LD F, Vx
     ///Set I = location of sprite for digit Vx.
     ///
     ///The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx. See section 2.4, Display, for more information on the Chip-8 hexadecimal font.
-    pub fn OpFx29(self: *Cpu, opcode: Opcode) !void {}
+    fn OpFx29(self: *Cpu, opcode: Opcode) !void {}
+
     ///Fx33 - LD B, Vx
-    ///Store BCD representation of Vx in memory locations I, I+1, and I+2.
+    ///Store BCD repr
+    /// _ = opcode;esentation of Vx in memory locations I, I+1, and I+2. //TODO (this is the hardest one)
     ///
     ///The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.
-    pub fn OpFx33(self: *Cpu, opcode: Opcode) !void {}
+    fn OpFx33(self: *Cpu, opcode: Opcode) !void {}
+
     ///Fx55 - LD [I], Vx
     ///Store registers V0 through Vx in memory starting at location I.
-    ///
-    ///The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
-    pub fn OpFx55(self: *Cpu, opcode: Opcode) !void {}
+    //
+    /// if (self.keypad[GetX(opcode)]) {
+    /// self.pc += 2;}/
+    ///The interpreter copies the values of regi
+    /// _ = opcode;sters V0 through Vx into memory, starting at the address in I. //TODO (this is the hardest one)
+    fn OpFx55(self: *Cpu, opcode: Opcode) !void {}
+
     ///Fx65 - LD Vx, [I]
     ///Read registers V0 through Vx from memory starting at location I.
     ///
     ///The interpreter reads values from memory starting at location I into registers V0 through Vx.};
-    pub fn OpFx65(self: *Cpu, opcode: Opcode) !void {}
+    fn OpFx65(self: *Cpu, opcode: Opcode) !void {
+        if (self.keypad[GetX(opcode)]) {
+            self.pc += 2;
+        }
+    }
+
+    fn GetX(self: *Cpu, opcode: Opcode) !u8 {
+        return self.registers[Decode.x(opcode)];
+    }
+
+    fn GetY(self: *Cpu, opcode: Opcode) !u8 {
+        return self.registers[Decode.y(opcode)];
+    }
+
     fn rand() u8 {
         return std.crypto.random.int(u8);
     }
